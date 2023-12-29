b102eec7a396c9695a9a0a1d2a67bfe9392d090e
Luu Hung <hungluu344@gmail.com>
2023-12-29 17:40:21 +0700

    Add project files.

---
+++ NewYearFireworks/Game.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearFireworks
+{
+    class Game
+    {
+        public void Start()
+        {
+            Console.Title = "Fireworks";
+            RunMainMenu();
+        }
+        private void RunMainMenu()
+        {
+            string Prompt = @"
+ ________ .-./`) .-------.        .-''-.  .--.      .--.    ,-----.    .-------.    .--.   .--.   
+|        |\ .-.')|  _ _   \     .'_ _   \ |  |_     |  |  .'  .-,  '.  |  _ _   \   |  | _/  /    
+|   .----'/ `-' \| ( ' )  |    / ( ` )   '| _( )_   |  | / ,-.|  \ _ \ | ( ' )  |   | (`' ) /     
+|  _|____  `-'`""`|(_ o _) /   . (_ o _)  ||(_ o _)  |  |;  \  '_ /  | :|(_ o _) /   |(_ ()_)      
+|_( )_   | .---. | (_,_).' __ |  (_,_)___|| (_,_) \ |  ||  _`,/ \ _/  || (_,_).' __ | (_,_)   __  
+(_ o._)__| |   | |  |\ \  |  |'  \   .---.|  |/    \|  |: (  '\_/ \   ;|  |\ \  |  ||  |\ \  |  | 
+|(_,_)     |   | |  | \ `'   / \  `-'    /|  '  /\  `  | \ `""/  \  ) / |  | \ `'   /|  | \ `'   / 
+|   |      |   | |  |  \    /   \       / |    /  \    |  '. \_/``"".'  |  |  \    / |  |  \    /  
+'---'      '---' ''-'   `'-'     `'-..-'  `---'    `---`    '-----'    ''-'   `'-'  `--'   `'-'   
+                                                                                                  
+This game renders multiple fireworks
+(UP/DOWN: MOVE)
+(ENTER: SELECT)";
+            string[] Options = new string[] { "Play", "Credit", "Exit" };
+            Menu main = new Menu(Options, Prompt);
+            int selectedIndex = main.Run();
+            switch (selectedIndex)
+            {
+                case 2:
+                    Exit();
+                    break;
+                case 1:
+                    Credits();
+                    break;
+                case 0:
+                    break;
+            }
+        }
+        private void Exit()
+        {
+            Console.WriteLine("Good bye");
+        }
+        private void Credits()
+        {
+            Console.Clear();
+            string Credits = @"
+V<<----[[CREDIT]]---->>V  V<<<-----[[DESCRIPTION]]----->>>V
+|      |        |      |  |        |             |        |
+^______^________^______^  ^________^_____________^________^
+|Creator: Luu Thai Hung|  |This game is made to celebrate |
+|Date: 12/29/2023      |  |new year. It generates multiple|
+|Version: 1.1          |  |firewoks with different colors.|
+|______________________|  |_______________________________|
+";
+            Console.WriteLine(Credits);
+            Console.WriteLine("Press any key to go back to Main Menu");
+            Console.ReadKey();
+            Console.Clear();
+            RunMainMenu();
+        }
+    }
+}
---
+++ NewYearFireworks/Menu.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace NewYearFireworks
+{
+    class Menu
+    {
+        public Menu(string[] options, string prompt)
+        {
+            Options = options;
+            Prompt = prompt;
+            SelectedIndex = 0;
+        }
+        public int SelectedIndex;
+        public string[] Options { get; }
+        public string Prompt { get; }
+
+        private void Display()
+        {
+            string prefix = " ";
+            WriteLine(Prompt);
+            for (int i = 0; i < Options.Length; i++)
+            {
+                if (i == SelectedIndex)
+                {
+                    prefix = ">";
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    prefix = " ";
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine($"{prefix}[{Options[i]}] ");
+                ResetColor();
+            }
+        }
+        public int Run()
+        {
+            ConsoleKey keypressed;
+            do
+            {
+                SetCursorPosition(0, 0);
+                Display();
+                ConsoleKeyInfo key = Console.ReadKey();
+                keypressed = key.Key;
+
+                if (keypressed == ConsoleKey.UpArrow)
+                {
+                    if (SelectedIndex == 0)
+                        SelectedIndex = Options.Length - 1;
+                    else
+                        SelectedIndex--;
+                }
+                else if (keypressed == ConsoleKey.DownArrow)
+                {
+                    if (SelectedIndex == Options.Length - 1)
+                        SelectedIndex = 0;
+                    else
+                        SelectedIndex++;
+                }
+            } while (keypressed != ConsoleKey.Enter);
+            return SelectedIndex;
+        }
+    }
+}
