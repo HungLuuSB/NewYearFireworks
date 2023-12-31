7bb60f210749330078a3054616f907bcbbbb55e6
Luu Hung <hungluu344@gmail.com>
2023-12-31 10:05:37 +0700

    Added firework class Added functionality to the game

---
+++ NewYearFireworks/Firework.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearFireworks
+{
+    class Firework
+    {
+        private readonly int startPosX;
+        private readonly int startPosY;
+        public int ColorNum { get; set; }
+        public int PositionX { get; set; }
+        public int PositionY { get; set; }
+        public int DenoteY { get; set; }
+        public int[,] Board { get; }
+        public Firework(int[,] board, int StartPosX, int StartPosY, int color)
+        {
+            Board = board;
+            startPosX = StartPosX;
+            startPosY = StartPosY;
+            ColorNum = color;
+            PositionX = StartPosX;
+            PositionY = StartPosY;
+            Random random = new Random();
+            DenoteY = random.Next(5, 15);
+        }
+        public void Explode()
+        {
+            //Exploding
+            /*
+               ↖ ↑ ↗
+             ← ← O → →
+               ↙ ↓ ↘
+            3 -> Center
+            4 - > UpArrow
+            5-> LeftUpArrow
+            6 -> RightUpArrow
+            7 -> LeftArrow
+            8 -> RightArrow
+            9 -> DownArrow
+            10 -> LeftDownArrow
+            11 -> RightDownArrow
+            */
+            //First layer
+            Board[PositionY, PositionX] = 3 + ColorNum; //Center
+
+            Board[PositionY - 2, PositionX] = 4 + ColorNum; //UpArrow
+            Board[PositionY - 2, PositionX - 2] = 5 + ColorNum; //UpLeftArrow
+            Board[PositionY - 2, PositionX + 2] = 6 + ColorNum; //UpRightArrow
+
+            Board[PositionY, PositionX - 2] = 7 + ColorNum; //LeftArrow 
+            Board[PositionY, PositionX + 2] = 8 + ColorNum; //Rightrrow
+
+            Board[PositionY + 2, PositionX] = 9 + ColorNum; //DownArrow
+            Board[PositionY + 2, PositionX - 2] = 10 + ColorNum; //DownLeftArrow
+            Board[PositionY + 2, PositionX + 2] = 11 + ColorNum; //DownRightArrow
+            Thread.Sleep(500);
+            //Second layer
+            Board[PositionY - 4, PositionX] = 4 + ColorNum; //UpArrow
+            Board[PositionY - 4, PositionX - 4] = 5 + ColorNum; //UpLeftArrow
+            Board[PositionY - 4, PositionX + 4] = 6 + ColorNum; //UpRightArrow
+
+            Board[PositionY, PositionX - 4] = 7 + ColorNum; //LeftArrow
+            Board[PositionY, PositionX + 4] = 8 + ColorNum; //RightArrow
+
+            Board[PositionY + 4, PositionX] = 9 + ColorNum; //DownArrow
+            Board[PositionY + 4, PositionX - 4] = 10 + ColorNum; //DownLeftArrow
+            Board[PositionY + 4, PositionX + 4] = 11 + ColorNum; //DownRightArrow
+            Thread.Sleep(500);
+            //Final
+            Board[PositionY, PositionX] = 0; //Center
+
+            Board[PositionY - 2, PositionX] = 0; //UpArrow
+            Board[PositionY - 2, PositionX - 2] = 0; //UpLeftArrow
+            Board[PositionY - 2, PositionX + 2] = 0; //UpRightArrow
+
+            Board[PositionY - 4, PositionX] = 0; //UpArrow2
+            Board[PositionY - 4, PositionX - 4] = 0; //UpLeftArrow2
+            Board[PositionY - 4, PositionX + 4] = 0; //UpRightArrow2
+
+            Board[PositionY, PositionX - 2] = 0; //LeftArrow 
+            Board[PositionY, PositionX - 4] = 0; //LeftArrow
+            Board[PositionY, PositionX + 2] = 0; //Rightrrow
+            Board[PositionY, PositionX + 4] = 0; //RightArrow
+
+            Board[PositionY + 2, PositionX] = 0; //DownArrow
+            Board[PositionY + 2, PositionX - 2] = 0; //DownLeftArrow
+            Board[PositionY + 2, PositionX + 2] = 0; //DownRightArrow
+
+            Board[PositionY + 4, PositionX] = 0; //DownArrow2
+            Board[PositionY + 4, PositionX - 4] = 0; //DownLeftArrow2
+            Board[PositionY + 4, PositionX + 4] = 0; //DownRightArrow2
+        }
+        public void Fire()
+        {
+            Board[PositionY, PositionX] = 2 + ColorNum;
+            Board[PositionY--, PositionX] = 1 + ColorNum;
+            while (PositionY >= DenoteY)
+            {
+                //Board[PositionY-=2, PositionX] = 0;
+                Board[PositionY + 1, PositionX] = 0;
+                Board[PositionY, PositionX] = 0;
+                PositionY--;
+                Board[PositionY, PositionX] = 2 + ColorNum;
+                Board[PositionY + 1, PositionX] = 1 + ColorNum;
+                Thread.Sleep(500);
+            }
+            Board[PositionY, PositionX] = 3 + ColorNum;
+            Board[PositionY + 1, PositionX] = 0;
+            Thread explode = new Thread(() => {
+                Explode();
+            });
+            explode.Start();
+        }
+        public void BeginFiring()
+        {
+            Thread firing = new Thread(() => {
+                Fire();
+            });
+            firing.Start();
+        }
+    }
+}
+
--- NewYearFireworks/Game.cs
+++ NewYearFireworks/Game.cs
@@ -8,6 +8,7 @@ namespace NewYearFireworks
 {
     class Game
     {
+        public bool Gameover = false;
         public void Start()
         {
             Console.Title = "Fireworks";
@@ -25,7 +26,7 @@ namespace NewYearFireworks
 |(_,_)     |   | |  | \ `'   / \  `-'    /|  '  /\  `  | \ `""/  \  ) / |  | \ `'   /|  | \ `'   / 
 |   |      |   | |  |  \    /   \       / |    /  \    |  '. \_/``"".'  |  |  \    / |  |  \    /  
 '---'      '---' ''-'   `'-'     `'-..-'  `---'    `---`    '-----'    ''-'   `'-'  `--'   `'-'   
-                                                                                                  
+(C) 2023 Luu Thai Hung. All rights reserved.                                                                                                  
 This game renders multiple fireworks
 (UP/DOWN: MOVE)
 (ENTER: SELECT)";
@@ -41,6 +42,7 @@ This game renders multiple fireworks
                     Credits();
                     break;
                 case 0:
+                    Begin();
                     break;
             }
         }
@@ -65,6 +67,200 @@ V<<----[[CREDIT]]---->>V  V<<<-----[[DESCRIPTION]]----->>>V
             Console.ReadKey();
             Console.Clear();
             RunMainMenu();
+        }
+        private void PrintScreen(int[,] Screen)
+        {
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < Screen.GetLength(0); i++)
+            {
+                for (int j = 0; j < Screen.GetLength(1); j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    switch (Screen[i , j])
+                    {
+                        case 0:
+                            Console.Write(" ");
+                            break;
+                        case 1:
+                            Console.Write("|");
+                            break;
+                        case 2:
+                            Console.Write("*");
+                            break;
+                        case 3:
+                            Console.Write("O");
+                            break;
+                        case 4:
+                            Console.Write("|");
+                            break;
+                        case 5:
+                            Console.Write("\\");
+                            break;
+                        case 6:
+                            Console.Write("/");
+                            break;
+                        case 7:
+                            Console.Write("-");
+                            break;
+                        case 8:
+                            Console.Write("-");
+                            break;
+                        case 9:
+                            Console.Write("|");
+                            break;
+                        case 10:
+                            Console.Write("/");
+                            break;
+                        case 11:
+                            Console.Write("\\");
+                            break;
+                        case 12:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("|");
+                            break;
+                        case 13:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("*");
+                            break;
+                        case 14:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("O");
+                            break;
+                        case 15:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("|");
+                            break;
+                        case 16:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("\\");
+                            break;
+                        case 17:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("/");
+                            break;
+                        case 18:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("-");
+                            break;
+                        case 19:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("-");
+                            break;
+                        case 20:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("|");
+                            break;
+                        case 21:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("/");
+                            break;
+                        case 22:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("\\");
+                            break;
+                        case 23:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("|");
+                            break;
+                        case 24:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("*");
+                            break;
+                        case 25:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("O");
+                            break;
+                        case 26:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("|");
+                            break;
+                        case 27:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("\\");
+                            break;
+                        case 28:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("/");
+                            break;
+                        case 29:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("-");
+                            break;
+                        case 30:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("-");
+                            break;
+                        case 31:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("|");
+                            break;
+                        case 32:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("/");
+                            break;
+                        case 33:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("\\");
+                            break;
+                    }
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+        static void InitFireworks(int[,] Board, int attempts)
+        {
+            Random random = new Random();
+            for (int i = 1; i <= attempts; i++)
+            {
+                int randomX = random.Next(5, 95);
+                int randomY = random.Next(20, 24);
+                int randomcolor = random.Next(0, 3);
+
+                int colornum = 0;
+                switch (randomcolor)
+                {
+                    case 0:
+                        colornum = 0;
+                        break;
+                    case 1:
+                        colornum = 11;
+                        break;
+                    case 2:
+                        colornum = 22;
+                        break;
+                    default: colornum = 0; break;
+                }
+                Firework fw = new Firework(Board, randomX, randomY, colornum);
+                fw.BeginFiring();
+                Thread.Sleep(random.Next(1000, 2000));
+            }
+        }
+        private void Begin()
+        {
+            Console.Clear();
+            int height = 25; int width = 100;
+            int[,] Screen = new int[height, width];
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Screen[i, j] = 0;
+                }
+            }
+            Console.Write("Input attempts: ");
+            int attempts = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Thread init = new Thread(() => { InitFireworks(Screen, attempts); });
+            init.Start();
+            Thread start = new Thread(() => {
+                while (true)
+                {
+                    PrintScreen(Screen);
+                    Thread.Sleep(500);
+                }
+            });
+            start.Start();
         }
     }
 }
